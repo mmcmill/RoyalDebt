8df7cd4c2b7014ae71b945e2b3257351b59caf9f
mmcmill <mmcmill@ncsu.edu>
2019-11-08 18:29:01 -0500

    Switched many things to prefabs, implemented walpole enemy.

---
+++ RoyalDebt/Assets/Scripts/HeadEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadEnemy : MonoBehaviour
+{
+    public float health;
+    public float damageTaken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            this.health -= damageTaken;
+            if(this.health < 0)
+            {
+                Destroy(gameObject);
+            }
+        } else
+        {
+            //Get the first contact point's normal
+            Vector2 normal = collision.contacts[0].normal;
+            ScriptedMovement2D mov = GetComponent<ScriptedMovement2D>();
+
+            if (normal.x != 0)
+            {
+                mov.xSpeed *= -1;
+            }
+            if (normal.y != 0)
+            {
+                mov.ySpeed *= -1;
+            }
+        }
+    }
+}
--- RoyalDebt/Assets/Scripts/ScriptedMovement2D.cs
+++ RoyalDebt/Assets/Scripts/ScriptedMovement2D.cs
@@ -7,6 +7,13 @@ public class ScriptedMovement2D : MonoBehaviour
     public bool tieSpeedToBall;
     public float xSpeed;
     public float ySpeed;
+    /// <summary>
+    /// X and Y values that define the range of movement that is allowed.
+    /// if both are left as zero, this will go unused.
+    /// ex: if movement limit is (5, 0), and the starting point is (0, 0) then X can go from (-2.5,0) to (2.5,0) 
+    /// </summary>
+    public Vector2 movementLimit;
+    private Vector2 _startPos;
     private float timer = 0.0f;
     private float waitTime = 0.2f;
     private SuperBall ball;
@@ -14,27 +21,52 @@ public class ScriptedMovement2D : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ball = GetComponentInChildren<SuperBall>();
-        SyncSpeedToBall();
+        if (tieSpeedToBall)
+        {
+            ball = GetComponentInChildren<SuperBall>();
+            SyncSpeedToBall();
+        }
+        _startPos = transform.localPosition;
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer > waitTime)
+        if (tieSpeedToBall)
         {
-            SyncSpeedToBall();
-            timer -= waitTime;
+            timer += Time.deltaTime;
+            if (timer > waitTime)
+            {
+                SyncSpeedToBall();
+                timer -= waitTime;
+            }
         }
     }
 
     void FixedUpdate()
     {
+        if (!movementLimit.Equals(Vector2.zero)){
+            LimitMovement();
+        }
+
         transform.Translate(new Vector3(xSpeed, ySpeed, 0));
+
     }
 
-    void SyncSpeedToBall()
+    private void LimitMovement()
+    {
+        Vector2 currPos = new Vector2(transform.localPosition.x, transform.localPosition.y);
+        Vector2 distance = _startPos - currPos;
+        if(distance.x > movementLimit.x/2.0f || distance.x < -movementLimit.x / 2.0f)
+        {
+            xSpeed *= -1;
+        }
+        if (distance.y > movementLimit.y / 2.0f || distance.y < -movementLimit.y / 2.0f)
+        {
+            ySpeed *= -1;
+        }
+    }
+
+    private void SyncSpeedToBall()
     {
         this.xSpeed = Mathf.Abs(ball.Speed.x);
         this.ySpeed = Mathf.Abs(ball.Speed.y);
